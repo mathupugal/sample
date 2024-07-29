3622ad9362e7b4b9a308e6b22dc697d953f1596c
mathupugal <mathupugal21@gmail.com>
2024-07-29 14:52:34 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,8 @@ for i in range(0, n):
     print(" " * (n-i), end=" ")
     for j in range(0, i+1):
         print(chr(alph), end=" ")
+        print()
+        print()
         alph += 1
     alph = 65
     print()
